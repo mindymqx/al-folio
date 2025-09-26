8f4b0ab98715ac3d89a711697ded651bb43aee49
mindymqx <92093465+mindymqx@users.noreply.github.com>
2025-09-26 09:51:54 +0000

    Deploying to gh-pages from @ mindymqx/al-folio@05c970e87b610418b91803723b391f4eadfc2226 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,51 +46,36 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-childhood-time-vr-relaxation",
+          title: 'Childhood time VR relaxation',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/al-folio/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-herbal",
+          title: 'Herbal',
+          description: "Product and Service Design",
           section: "Projects",handler: () => {
               window.location.href = "/al-folio/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
+            },},{id: "projects-windmill",
+          title: 'Windmill',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/al-folio/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
+            },},{id: "projects-reminiscence-building-animation",
+          title: 'Reminiscence building animation',
           description: "another without an image",
           section: "Projects",handler: () => {
               window.location.href = "/al-folio/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
+            },},{id: "projects-mind-map",
+          title: 'Mind Map',
           description: "a project with a background image",
           section: "Projects",handler: () => {
               window.location.href = "/al-folio/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
+            },},{id: "projects-virus",
+          title: 'Virus',
           description: "a project with no image",
           section: "Projects",handler: () => {
               window.location.href = "/al-folio/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/al-folio/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/al-folio/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/al-folio/projects/9_project/";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
